f47a15903530a2920f4cee5e6b878563925db29d
Maheedhar15 <maheedharsumson@gmail.com>
2022-10-01 15:41:19 +0530

    Finalized design

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
+import Nav from './components/nav/Nav'
 
 const App = () => {
   return (
     <>
       <Header />
+      <Nav />
       <Body />
       <Footer />
     </>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import './footer.css'
+import FooterSocials from './FooterSocials';
 
 const Footer = () => {
   return (
     <section id="footer">
+      <h1>Connect with us</h1>
       <div className="container footer__body">
-        <h2>This is the footer</h2>
+        <h2>Follow Us for great events</h2>
+        <div className="socials">
+          <h2>Here are our socials</h2>
+          <FooterSocials />
+        </div>
       </div>
     </section>
   );
